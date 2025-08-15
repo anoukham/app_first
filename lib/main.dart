a076ff49a6475d9008f6982554a623457c9452b1
Anoukham <aek.hongdala16@gmail.com>
2025-08-15 10:09:45 +0700

    text and container all style

--- lib/main.dart
+++ lib/main.dart
@@ -32,14 +32,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,21 +41,23 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text('$_counter', style: Theme.of(context).textTheme.headlineMedium),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.blue,
-        onPressed: _incrementCounter,
-        tooltip: 'ເພີ່ມ',
-        child: const Text(
-          'ເພຶ່ມຕັວເລກ',
-          style: TextStyle(fontSize: 12, color: Colors.white),
+        child: Container(
+          width: 200,
+          height: 100,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: Colors.lightBlueAccent,
+          ),
+          child: Center(
+            child: Text(
+              'Hello Flutter',
+              style: TextStyle(
+                fontSize: 24,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
         ),
       ),
     );
